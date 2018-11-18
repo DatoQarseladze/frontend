d76fef813d7685f76e627b6d90dd13f3e3ae5ea1
jrdleto1 <dqarseladze@gmail.com>
2018-11-18 11:44:35 +0400

    commit

---
+++ 16nov/react-demo/build/precache-manifest.c61c611596315713b431c69bc16cf845.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3309c0bc90566c7095",
+    "url": "/frontend/16nov/react-demo/static/js/runtime~main.2c3309c0.js"
+  },
+  {
+    "revision": "b886351798903629916f",
+    "url": "/frontend/16nov/react-demo/static/js/main.b8863517.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/frontend/16nov/react-demo/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "b886351798903629916f",
+    "url": "/frontend/16nov/react-demo/static/css/main.5f74d436.chunk.css"
+  },
+  {
+    "revision": "11543f234036a8b25bb23d084d5e90e6",
+    "url": "/frontend/16nov/react-demo/index.html"
+  }
+];
